f631c7c3a757b9784d36a98030b6be24ffdb9692
Sam Hamer <samuel.hamer@outlook.com>
2024-04-19 20:06:07 -0500

    part 2.1 to 2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,40 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+  return <p>Number of exercises{" " + total}</p>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
