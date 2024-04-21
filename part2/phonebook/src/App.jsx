1b4546bb8d21e62a42f66c9ad6b6059b8cbd9af5
Sam Hamer <samuel.hamer@outlook.com>
2024-04-20 20:20:33 -0500

    part 2.6 to 2.10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,71 @@
+import Filter from "./components/Filter";
+import { useState } from "react";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook.`);
+    } else {
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+      };
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const peopleToShow =
+    filter === ""
+      ? persons
+      : persons.filter(
+          (person) =>
+            person.name.toLowerCase().search(filter.toLocaleLowerCase()) !== -1
+        );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} onChange={handleFilterChange} />
+      <h2>add a new</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleSubmit={handleSubmit}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons peopleToShow={peopleToShow} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/phonebook/src/components/Person.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Person = ({ person }) => {
+  return (
+    <li key={person.name}>
+      {person.name} {person.number}
+    </li>
+  );
+};
+
+export default Person;
